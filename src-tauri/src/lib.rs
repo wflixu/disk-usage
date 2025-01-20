bc21dc3c2e66ebab458cd10cde409e9b61b428b3
wflixu <wflixu@88.com>
2025-01-20 21:46:24 +0800

    feat:open dir and get dir stat

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,116 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde::{Deserialize, Serialize};
+use std::process::Command;
+
+#[derive(Serialize)]
+pub struct CRes<T: Serialize> {
+    pub code: u16,
+    pub msg: String,
+    pub data: Option<T>,
+}
+
+impl<T: Serialize> CRes<T> {
+    pub fn success(data: Option<T>) -> Self {
+        CRes {
+            code: 200,
+            msg: String::from(""),
+            data,
+        }
+    }
+
+    pub fn fail(code: u16, msg: String) -> Self {
+        CRes {
+            code,
+            msg,
+            data: None,
+        }
+    }
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet(name: &str) -> CRes<String> {
+    CRes::success(Some(format!(
+        "Hello, {}! You've been greeted from Rust!",
+        name
+    )))
+}
+
+#[tauri::command]
+fn open_dir(path: &str) -> CRes<()> {
+    let result = if cfg!(target_os = "macos") {
+        Command::new("open")
+            .arg(path)
+            .spawn()
+            .and_then(|mut child| child.wait())
+    } else if cfg!(target_os = "windows") {
+        Command::new("explorer")
+            .arg(path)
+            .spawn()
+            .and_then(|mut child| child.wait())
+    } else if cfg!(target_os = "linux") {
+        Command::new("xdg-open")
+            .arg(path)
+            .spawn()
+            .and_then(|mut child| child.wait())
+    } else {
+        return CRes::fail(1, String::from("Unsupported OS"));
+    };
+
+    match result {
+        Ok(_) => CRes::success(None),
+        Err(e) => CRes::fail(1, e.to_string()),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct DirOrFile {
+    name: String,
+    is_dir: bool,
+    count: u64,
+}
+
+#[tauri::command]
+fn dir_stat(path: &str) -> CRes<Vec<DirOrFile>> {
+    use std::fs;
+    use std::path::Path;
+
+    fn get_dir_stat(path: &Path) -> Vec<DirOrFile> {
+        let mut entries = vec![];
+        if let Ok(read_dir) = fs::read_dir(path) {
+            for entry in read_dir {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    let metadata = fs::metadata(&path).unwrap();
+                    let is_dir = metadata.is_dir();
+                    let count = if is_dir {
+                        get_dir_stat(&path).iter().map(|e| e.count).sum()
+                    } else {
+                        metadata.len()
+                    };
+                    entries.push(DirOrFile {
+                        name: entry.file_name().into_string().unwrap(),
+                        is_dir,
+                        count,
+                    });
+                }
+            }
+        }
+        entries
+    }
+
+    let path = Path::new(path);
+    if !path.exists() {
+        return CRes::fail(1, String::from("Path does not exist"));
+    }
+
+    let stats = get_dir_stat(path);
+    CRes::success(Some(stats))
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, open_dir, dir_stat])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -5,45 +5,35 @@ import { invoke } from "@tauri-apps/api/core";
 const greetMsg = ref("");
 const name = ref("");
 
+const url = ref("/Users/lixu/play/")
+
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
+  let res  = await invoke("open_dir", { path: url.value });
+  console.log(res)
+}
+async function dirstat() {
+  let res  = await invoke("dir_stat", { path: url.value });
+  console.log(res)
 }
 </script>
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <section>
+      <input type="text" v-model="url">
+      <button @click="greet">open</button>
+      <button @click="dirstat">stat</button>
+    </section>
+    <section>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
+    </section>
+     
   </main>
 </template>
 
 <style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
 
 </style>
 <style>
@@ -65,23 +55,10 @@ async function greet() {
 
 .container {
   margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
+  display: grid;
+ 
 }
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
 
 .row {
   display: flex;
